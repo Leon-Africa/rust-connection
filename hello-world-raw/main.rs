60e3bbb3bde2602376f5792457b1ef61ebf3541e
Leon-Africa <mr.leon.africa@gmail.com>
2025-04-07 05:04:31 +0200

    hello world raw rust program

---
+++ hello-world-raw/main.rs
@@ -0,0 +1,5 @@
+// A rust program not created with cargo needs to be compiled each time with rustc
+// Also it will not have the base structure provided when creating a project with Cargo
+fn main() {
+    println!("Hello, world!")
+}
